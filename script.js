f19cc42aab5b5ba6efe8d965cf6ddc635b834187
ashib-hw <flagpandagamingadx01@gmail.com>
2025-08-04 15:11:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnE1 = document.querySelector(".btn")
+
+btnE1.addEventListener("mouseover", (e)=>{
+
+    const x = e.pageX - btnE1.offsetLeft;
+    const y = e.pageY - btnE1.offsetTop;
+
+    btnE1.style.setProperty("--posX", x + "px")
+    btnE1.style.setProperty("--posY", y + "px")
+})
